039eb38110534306eaaa0b3685793c86cb22107c
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-01-22 12:09:38 -0600

    add Loader

--- js/pokedex.js
+++ js/pokedex.js
@@ -2,6 +2,7 @@ import { getPokemon, getPokemonSpecies } from './api.js';
 
 const $image = document.querySelector('#image');
 const $description = document.querySelector('#description');
+const $screen = document.querySelector('#screen');
 
 const setImage = (img) => {
     $image.src = img;
@@ -9,6 +10,11 @@ const setImage = (img) => {
 
 const setDesc = (text) => {
     $description.innerText = text;
+};
+
+const loader = (isLoading = false) => {
+    const img = isLoading ? 'url(./images/loading.gif)' : '';
+    $screen.style.backgroundImage = img;
 };
 
 export const findPokemon = async (id) => {
@@ -26,7 +32,9 @@ export const findPokemon = async (id) => {
 };
 
 export const setPokemon = async (id) => {
+    loader(true); // loader
     const { sprites, description } = await findPokemon(id);
+    loader(false); // quitar loader
 
     setImage(sprites);
     setDesc(description);
